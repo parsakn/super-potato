720cbe30b7102e30ea48a84d7f26e4fb0c3a5403
parsa <pkhoshnazari@gmail.com>
2023-12-03 19:03:38 +0330

    The Clean structured code of the game.

---
+++ Src/Bomberman.cpp
@@ -0,0 +1,84 @@
+#include "Bomberman.h"
+
+void Bomberman::initVariables() {
+    this->livesMax = MAX_LIVES;
+    this->movementSpeed = 2.0f;
+}
+
+void Bomberman::initTexture() {
+
+    if (!this->upTexture.loadFromFile(BMAN_UP_TEXT))
+    {
+        std::cout << "ERROR::PLAYER::INITTEXTURE::Could not load uptexture file." << "\n";
+    }
+    if (!this->downTexture.loadFromFile(BMAN_DOWN_TEXT))
+    {
+        std::cout << "ERROR::PLAYER::INITTEXTURE::Could not load downtexture file." << "\n";
+    }
+    if (!this->rightTexture.loadFromFile(BMAN_RIGHT_TEXT))
+    {
+        std::cout << "ERROR::PLAYER::INITTEXTURE::Could not load righttexture file." << "\n";
+    }
+    if (!this->leftTexture.loadFromFile(BMAN_LEFT_TXT))
+    {
+        std::cout << "ERROR::PLAYER::INITTEXTURE::Could not load lefttexture file." << "\n";
+    }
+
+}
+
+void Bomberman::initSprite() {
+
+    this->sprite.setTexture(this->downTexture);
+
+}
+
+Bomberman::Bomberman() {
+
+    this->initVariables();
+    this->initTexture();
+    this->initSprite();
+}
+
+Bomberman::~Bomberman() {
+
+}
+
+void Bomberman::update() {
+
+}
+
+void Bomberman::render(sf::RenderTarget &target) {
+    target.draw(this->sprite);
+}
+
+void Bomberman::move(const float dirX, const float dirY) {
+    this->sprite.move(this->movementSpeed * dirX, this->movementSpeed * dirY);
+}
+
+void Bomberman::setPosition(const float x, const float y) {
+    this->sprite.setPosition(x, y);
+}
+
+void Bomberman::setScale(const float xScale, const float yScale) {
+    this->sprite.setScale(xScale,yScale);
+}
+
+void Bomberman::changeDirection(std::string direction) {
+
+    if (direction == "Right"){
+        this->sprite.setTexture(this->rightTexture);
+    }
+    if (direction == "Left"){
+        this->sprite.setTexture(this->leftTexture);
+    }
+    if (direction == "Up"){
+        this->sprite.setTexture(this->upTexture);
+    }
+    if (direction == "Down"){
+        this->sprite.setTexture(this->downTexture);
+    }
+}
+
+
+
+
---
+++ Src/Bomberman.h
@@ -0,0 +1,51 @@
+#ifndef BOMBERMAN_V5_BOMBERMAN_H
+#define BOMBERMAN_V5_BOMBERMAN_H
+
+#include <SFML/Graphics.hpp>
+#include "iostream"
+#include "Manual.h"
+#include "string"
+#include "vector"
+
+
+
+class Bomberman {
+private:
+    sf::Sprite sprite;
+    sf::Texture upTexture;
+    sf::Texture downTexture;
+    sf::Texture rightTexture;
+    sf::Texture leftTexture;
+
+    float movementSpeed;
+
+    int livesMax;
+
+    //Private functions
+    void initVariables();
+    void initTexture();
+    void initSprite();
+
+
+public:
+
+    Bomberman();
+    virtual ~Bomberman();
+
+
+    //Modifies
+    void setPosition(const float x, const float y);
+    void setScale(const float xScale,const float yScale);
+    void changeDirection(std::string direction);
+
+
+    //Functions
+    void move(const float dirX, const float dirY);
+    void update();
+    void render(sf::RenderTarget& target);
+
+
+};
+
+
+#endif //BOMBERMAN_V5_BOMBERMAN_H
